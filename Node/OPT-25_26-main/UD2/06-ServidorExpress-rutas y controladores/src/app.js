4fd132a780b4622849149589372b54622864cba4
Cristina Simon <sihercris@gamil.com>
2025-11-19 08:21:16 +0100

    fin del 18/11/25

--- Node/OPT-25_26-main/UD2/06-ServidorExpress-rutas y controladores/src/app.js
+++ Node/OPT-25_26-main/UD2/06-ServidorExpress-rutas y controladores/src/app.js
@@ -14,6 +14,8 @@ server.get("/",(req,res)=>{
     res.status(200).send("Hola mundo servidor de Node.js funcionando con Nodemon!"); //Este es el mensaje que mostrara al final de la comunicacion y se mostrara en el navegador
 
 });
+server.use(express.json());//parsear a JSON en el body
+
 server.use ("/api", routerUsuarios)
 server.use((req, res)=>{
     res.status(404).send("Pagina no encontrada");
--- Node/OPT-25_26-main/UD2/06-ServidorExpress-rutas y controladores/src/controler/usuario.controler.js
+++ Node/OPT-25_26-main/UD2/06-ServidorExpress-rutas y controladores/src/controler/usuario.controler.js
@@ -1,40 +1,85 @@
-"use strict"
-import { usuarios } from "../data/usuarios.js"
+"use strict";
+import { usuarios } from "../data/usuarios.js";
 
-export const getUsuarios=(req,res)=>{
-    res.status(200).json({
-        data: usuarios,
-        total: usuarios.length
-    })
-}
-export const getUsuario=(req,res)=>{
-    console.log(req.params);
-    const {id}=req.params
-    if(isNaN(id)){
-        res.status(400).json({
-            message: 'el id debe ser numerico'
-        })
-    }
-    const indice =usuarios.findIndex(usuario=>usuario.id==id);
-    if(indice==-1){
-        res.status(400).json({
-            message: `el id ${id} no encontrado`
-        })
-    }
+export const getUsuarios = (req,  res) => {
+  return res.status(200).json({
+    data: usuarios,
+    total: usuarios.length,
+  });
+};
+export const getUsuario = (req,  res) => {
+  console.log(req.params);
+  const { id } = req.params;
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: "el id debe ser numerico",
+    });
+  }
+  // busqueda del id del usuario
+  const indice = usuarios.findIndex((usuario) => usuario.id == id);
+  if (indice == -1) {
+    return res.status(400).json({
+      message: `el id ${id} no encontrado`,
+    });
+  }
+  //si todo es correcto
+  return res.status(200).json({
+    data: usuarios.at(indice),
+  });
+};
+//POST - Crear un usuario
+export const addUsuarios = (req,  res) => {
+  console.log(req.body);
+  const { id, nombre, email, edad } = req.body;
+  if (
+    id.length == 0 ||
+    nombre.length == 0 ||
+    email.length == 0 ||
+    edad.length == 0
+  ) {
+    return res.status(400).json({
+      message: "los campos no pueden estar vacios",
+    });
+  }
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: "el id debe ser numerico",
+    });
+  }
+  const emailExist = usuarios.find((user) => user.email == email);
+  if (emailExist) {
+    return res.status(400).json({
+      message: "Ya exiiste un usuario con ese email",
+    });
+  } else {
+    usuarios.push(req.body);
+
+    return res.status(201).json({
+      message: `El usuario con ID ${id} ha sido grabado`,
+    });
+  }
+};
+export const updateUsuarios = (req,  res) => {};
+export const patchUsuarios = (req,  res) => {};
+export const detUsuarios = (req,  res) => {
+  const { id } = req.params;
+  if (isNaN(id)) {
+    return res.status(400).json({
+      message: "el id debe ser numerico",
+    });
+  }
+  // busqueda del id del usuario
+  const indice = usuarios.findIndex((usuario) => usuario.id == id);
+  if (indice == -1) {
+    return res.status(400).json({
+      message: `el id ${id} no encontrado`,
+    });
+  } else {
     //si todo es correcto
-    res.status(200).json({
-        data: usuarios.at(indice)
-    })
-}
-export const addUsuarios=(req,res)=>{
+    usuarios.splice(indice, 1);
 
-}
-export const updateUsuarios=(req,res)=>{
-
-}
-export const patchUsuarios=(req,res)=>{
-
-}
-export const detUsuarios=(req,res)=>{
-
-}
+    return res.status(200).json({
+      message: "usuario a sido eliminado",
+    });
+  }
+};
