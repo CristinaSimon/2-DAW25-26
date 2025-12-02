49cc1d5c40027392b2900d16b61990a4a3437187
Cristina Simon <sihercris@gamil.com>
2025-12-02 14:42:05 +0100

    fin 2/12/25

--- Cliente/Ejercicios/Ejercicio UD6/ejerFormas/js/app.js
+++ Cliente/Ejercicios/Ejercicio UD6/ejerFormas/js/app.js
@@ -1,32 +1,58 @@
-"use strict"
+"use strict";
+import { colors, sizes } from "./btnconten.js";
+const juegoformas = (() => {
+  /**
+   * declaracion de Constantes referenciadas al html
+   */
+  const panelcolor = document.querySelector("#colorSelector");
+  const paneltamano = document.querySelector("#sizeSelector");
+  const btnCrear = document.querySelector("#createBtn");
+  const btnClear = document.querySelector("#clearBtn");
+  const init = () => {
+    document.addEventListener("DOMContentLoaded", () => {
+      creaBtn();
+      btnCrear.addEventListener("click", crearObje);
+    });
+  };
 
-const panelcolor=document.querySelector("#colorSelector")
-    const colors = [
-        { name: 'Azul', class: 'color-blue' },
-        { name: 'Rojo', class: 'color-red' },
-        { name: 'Verde', class: 'color-green' },
-        { name: 'Amarillo', class: 'color-yellow' },
-        { name: 'Púrpura', class: 'color-purple' },
-        { name: 'Rosa', class: 'color-pink' }
-    ];
+  const creaBtn = () => {
+    colors.forEach((color) => {
+      const btn = document.createElement("button");
+      btn.setAttribute("name", color.name);
+      btn.classList.add(color.class);
+      btn.textContent = `${color.name}`;
+      btn.classList.add("color-btn");
+      btn.addEventListener("click", cambiarSelectColor);
+      panelcolor.append(btn);
+    });
+    //
+    panelcolor.children[0].classList.add("selected");
 
-    const sizes = [
-        { name: 'Pequeño', value: 'size-small' },
-        { name: 'Mediano', value: 'size-medium' },
-        { name: 'Grande', value: 'size-large' }
-    ];
-   
-    const creaBtnColor =()=>{
-        const btn=document.createElement("button");
-        colors.forEach(color => {
-            btn.getAttribute("name", color.name);
-            btn.classList.add(color.class);
-            btn.getAttribute("height", "30px")
-        });
-        
-        panelcolor.append(btn);
+    sizes.forEach((tamano) => {
+      const btn = document.createElement("button");
+      btn.setAttribute("name", tamano.name);
+      btn.setAttribute("value", tamano.value);
+      btn.textContent = `${tamano.name}`;
+      btn.addEventListener("click", cambiarSelectSice);
+      btn.classList.add("size-btn");
+      paneltamano.append(btn);
+    });
 
-
+    paneltamano.children[1].classList.add("selected");
+  };
+  const crearObje = () => {};
+  const cambiarSelectSice=(e)=>{
+    const ante=panelcolor.children.classList.contains("selected");
+    if(ante){
+        ante.classList.remove("selected");
     }
+    
+    e.target.classList.add("selected");
+  }
+  const cambiarSelectColor=()=>{}
 
-    creaBtnColor()
+  return {
+    init,
+  };
+})();
+juegoformas.init();
---
+++ Cliente/Ejercicios/Ejercicio UD6/ejerFormas/js/btnconten.js
@@ -0,0 +1,15 @@
+"use strict";
+export const colors = [
+  { name: "Azul", class: "color-blue" },
+  { name: "Rojo", class: "color-red" },
+  { name: "Verde", class: "color-green" },
+  { name: "Amarillo", class: "color-yellow" },
+  { name: "Púrpura", class: "color-purple" },
+  { name: "Rosa", class: "color-pink" },
+];
+
+export const sizes = [
+  { name: "Pequeño", value: "size-small" },
+  { name: "Mediano", value: "size-medium" },
+  { name: "Grande", value: "size-large" },
+];
