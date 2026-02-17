0cedfddf26182e48aae5d14efea96c9d67258412
Cristina Simon <sihercris@gamil.com>
2026-02-17 14:39:42 +0100

    17/2

---
+++ Cliente/Ejercicios/Ejercicio UD8/EjercicioAngular/src/app/mostrarestadisticas/mostrarestadisticas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mostrarestadisticas',
+  imports: [],
+  templateUrl: './mostrarestadisticas.html',
+  styleUrl: './mostrarestadisticas.css',
+})
+export class Mostrarestadisticas {
+
+}
